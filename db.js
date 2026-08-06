ccab73f3b4b14181ce6262bd286fa5a8043245d3
Taraftarium2 <beerzacarri55980@gmail.com>
2026-08-06 17:34:25 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,8 +1,17 @@
 const { Pool } = require('pg');
 
+const connectionString =
+  process.env.DATABASE_URL ||
+  process.env.POSTGRES_URL ||
+  process.env.DATABASE_PUBLIC_URL ||
+  process.env.PGURL;
+
+const isRailway = !!(process.env.RAILWAY_ENVIRONMENT || process.env.RAILWAY_STATIC_URL);
+const useSSL = process.env.PGSSL === 'true' || isRailway;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.PGSSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  connectionString,
+  ssl: useSSL ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = pool;
