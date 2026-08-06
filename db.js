c9c021dfc8bb22aaba1df26cf02b5c923b50e79a
Taraftarium2 <beerzacarri55980@gmail.com>
2026-08-06 17:43:22 +0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,10 +1,19 @@
 const { Pool } = require('pg');
 
-const connectionString =
+let connectionString =
   process.env.DATABASE_URL ||
   process.env.POSTGRES_URL ||
   process.env.DATABASE_PUBLIC_URL ||
-  process.env.PGURL;
+  process.env.PGURL ||
+  '';
+
+// Yanlışlıkla yapıştırılan "railwaypostgresql://" gibi hatalı ön ekleri otomatik temizle
+if (connectionString) {
+  const match = connectionString.match(/(postgres(?:ql)?:\/\/.+)$/i);
+  if (match) {
+    connectionString = match[1];
+  }
+}
 
 const isRailway = !!(process.env.RAILWAY_ENVIRONMENT || process.env.RAILWAY_STATIC_URL);
 const useSSL = process.env.PGSSL === 'true' || isRailway;
